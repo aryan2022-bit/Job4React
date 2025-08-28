007e2e264e02095118ec4475469b1a673d53be95
aryan2022-bit <aryanprasad3195@gmail.com>
2025-08-29 00:00:32 +0530

    Initial commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { RingLoader } from 'react-spinners'
+
+const override = {
+    display: 'block',
+    margin: '100px auto'
+}
+const Spinner = ({ loading }) => {
+  return (
+    <RingLoader
+        color='#4338ca'
+        loading={loading}
+        cssOverride={override}
+        size={150}
+    />
+  )
+}
+
+export default Spinner
