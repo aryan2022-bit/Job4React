56e76bd320e46a3002baed577d54b896c7be6cae
aryan2022-bit <aryanprasad3195@gmail.com>
2025-08-30 19:22:35 +0530

    Normalize line endings

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api':{
-        // target: 'http://localhost:5000',
-        target: 'https://job4react-json-server.onrender.com',
+        target: 'http://localhost:5000',
+        // target: 'https://job4react-json-server.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/,'')
       }
